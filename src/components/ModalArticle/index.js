23b61c83797b7de932ddbee7336814f3e5cb22da
datnt <39941143+DatNguyenCore@users.noreply.github.com>
2023-11-30 17:17:27 +0700

    update data

--- src/components/ModalArticle/index.js
+++ src/components/ModalArticle/index.js
@@ -1,37 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import Modal from "../Modal";
 
 export default function ModalArticle({
   children,
   visible = false,
   onCloseModal,
-  onOpenModal,
 }) {
-  // const [visible, setVisible] = useState(false);
-
-  // const closeModal = () => {
-  //   setVisible(false);
-  // };
-
-  // function onOpenModal() {
-  //   setVisible(true);
-  // }
-
   return (
-    <>
-      {/* <button type="button" onClick={onOpenModal}>
-        Click Me{" "}
-      </button> */}
-      <Modal closeModal={onCloseModal} isOpen={visible}>
-        <div className="w-screen h-screen max-w-5xl max-h-[90vh] flex flex-col items-center">
-          <div className="w-full py-4 text-xl font-light text-center font-medium">
-            Post title
-          </div>
-          <div className="w-full h-full px-10 text-black-800 overflow-auto pb-6">
-            {children}
-          </div>
+    <Modal closeModal={onCloseModal} isOpen={visible}>
+      <div className="w-screen h-screen max-w-5xl max-h-[90vh] flex flex-col items-center">
+        <div className="w-full py-4 text-xl font-light text-center font-medium">
+          KOVi
         </div>
-      </Modal>
-    </>
+        <div className="w-full h-full px-10 text-black-800 overflow-auto pb-6">
+          {children}
+        </div>
+      </div>
+    </Modal>
   );
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -21,7 +21,7 @@ import { auth, db } from "../../lib/firebase";
 import { GlobalContext } from "../../state/context/GlobalContext";
 import ModalArticle from "../ModalArticle";
 
-const Post = ({ id, username, image, caption, likesCount }) => {
+const Post = ({ id, username, image, caption, likesCount, desc, images }) => {
   const [visible, setVisible] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
   const [comments, setComments] = useState([]);
@@ -224,34 +224,19 @@ const Post = ({ id, username, image, caption, likesCount }) => {
         onCloseModal={onCloseModal}
         onOpenModal={onOpenModal}
       >
-        <p>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and
-          Evil) by Cicero, written in 45 BC. This book is a treatise on the
-          theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in
-          section 1.10.32.
-        </p>
-        <p>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and
-          Evil) by Cicero, written in 45 BC. This book is a treatise on the
-          theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in
-          section 1.10.32.
-        </p>
+        <div>
+          <div className="grid grid-rows-3 grid-flow-col gap-4">
+            {Array.isArray(images) &&
+              images.map((item) => {
+                return (
+                  <div key={item}>
+                    <img src={image} alt={caption} className="object-cover" />
+                  </div>
+                );
+              })}
+          </div>
+          <p className="mt-2">{desc}</p>
+        </div>
       </ModalArticle>
     </>
   );
