164476607827f9d487ecc353b378d0a40e1afdf1
datnt <39941143+DatNguyenCore@users.noreply.github.com>
2023-11-30 16:40:06 +0700

    update post

--- src/components/ModalArticle/index.js
+++ src/components/ModalArticle/index.js
@@ -1,47 +1,28 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import Modal from "../Modal";
-import {
-  GlobalContext,
-  GlobalDispatchContext,
-} from "../../state/context/GlobalContext";
 
-export default function ModalArticle({ children }) {
-  // const { isDetailPostModalOpen } = useContext(GlobalContext);
-  // const dispatch = useContext(GlobalDispatchContext);
-  const [visible, setVisible] = useState(false);
+export default function ModalArticle({
+  children,
+  visible = false,
+  onCloseModal,
+  onOpenModal,
+}) {
+  // const [visible, setVisible] = useState(false);
 
-  const closeModal = () => {
-    // dispatch({
-    //   type: "SET_IS_DETAIL_POST_MODAL_OPEN",
-    //   payload: {
-    //     isDetailPostModalOpen: false,
-    //   },
-    // });
+  // const closeModal = () => {
+  //   setVisible(false);
+  // };
 
-    setVisible(false);
-  };
-
-  function onOpenModal() {
-    setVisible(true);
-  }
+  // function onOpenModal() {
+  //   setVisible(true);
+  // }
 
   return (
     <>
-      <button
-        type="button"
-        // onClick={() =>
-        //   dispatch({
-        //     type: "SET_IS_DETAIL_POST_MODAL_OPEN",
-        //     payload: {
-        //       isDetailPostModalOpen: true,
-        //     },
-        //   })
-        // }
-        onClick={onOpenModal}
-      >
+      {/* <button type="button" onClick={onOpenModal}>
         Click Me{" "}
-      </button>
-      <Modal closeModal={closeModal} isOpen={visible}>
+      </button> */}
+      <Modal closeModal={onCloseModal} isOpen={visible}>
         <div className="w-screen h-screen max-w-5xl max-h-[90vh] flex flex-col items-center">
           <div className="w-full py-4 text-xl font-light text-center font-medium">
             Post title
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -19,10 +19,20 @@ import { FaRegComment } from "react-icons/fa";
 import { IoShareOutline } from "react-icons/io5";
 import { auth, db } from "../../lib/firebase";
 import { GlobalContext } from "../../state/context/GlobalContext";
+import ModalArticle from "../ModalArticle";
 
 const Post = ({ id, username, image, caption, likesCount }) => {
+  const [visible, setVisible] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
   const [comments, setComments] = useState([]);
+
+  const onCloseModal = () => {
+    setVisible(false);
+  };
+
+  function onOpenModal() {
+    setVisible(true);
+  }
 
   const handleLikePost = async () => {
     const postLike = {
@@ -109,103 +119,141 @@ const Post = ({ id, username, image, caption, likesCount }) => {
   };
 
   return (
-    <div className="flex flex-col w-full border border-gray-200">
-      <div className="flex items-center justify-between w-full p-2 ">
-        <div className="flex items-center justify-center space-x-2">
-          <div className="w-10 h-10 bg-black border-2 rounded-full" />
-          <div>{username}</div>
-        </div>
-        <div className="w-4 select-none">
-          <BsThreeDots className="text-lg" />
-        </div>
-      </div>
-      <div
-        className="relative flex items-center justify-center bg-black aspect-video"
-        style={{
-          height: "auto",
-          minHeight: "100%",
-        }}
-      >
-        <Image
-          src={image}
-          layout="fill"
-          alt={caption}
-          className="object-cover"
-        />
-      </div>
-      <div className="flex justify-between p-2 text-lg">
-        <div className="flex space-x-2">
-          <div onClick={handleLikePost}>
-            {isLiked ? (
-              <AiFillHeart
-                size={25}
-                className="text-red-500 cursor-pointer hover:text-red-500/50"
-              />
-            ) : (
-              <AiOutlineHeart
-                size={25}
-                className="text-black cursor-pointer hover:text-black/50"
-              />
-            )}
+    <>
+      <div className="flex flex-col w-full border border-gray-200">
+        <div className="flex items-center justify-between w-full p-2 ">
+          <div className="flex items-center justify-center space-x-2">
+            <div className="w-10 h-10 bg-black border-2 rounded-full" />
+            <div>{username}</div>
           </div>
-          <div>
-            <FaRegComment
-              size={22}
-              className="text-black cursor-pointer hover:text-black/50"
-            />
-          </div>
-          <div>
-            <IoShareOutline
-              size={22}
-              className="text-black cursor-pointer hover:text-black/50"
-            />
+          <div className="w-4 select-none">
+            <BsThreeDots className="text-lg" />
           </div>
         </div>
-        <div>
-          <BsBookmark
-            size={20}
-            className="text-black cursor-pointer hover:text-black/50"
+        <div
+          className="relative flex items-center justify-center bg-black aspect-video"
+          style={{
+            height: "auto",
+            minHeight: "100%",
+          }}
+          onClick={onOpenModal}
+        >
+          <Image
+            src={image}
+            layout="fill"
+            alt={caption}
+            className="object-cover"
           />
         </div>
-      </div>
-      <div className="px-2">
-        {likesCount ? `${likesCount} likes` : "Be the first to like"}
-      </div>
-      <div className="px-2">{caption}</div>
-      <div className="p-2">
-        <div className="flex flex-col space-y-1">
-          {comments.map((commentData) => (
-            <div key={commentData.id} className="flex space-x-2">
-              <div className="font-medium">{commentData.username}</div>
-              <div>{commentData.comment}</div>
+        <div className="flex justify-between p-2 text-lg">
+          <div className="flex space-x-2">
+            <div onClick={handleLikePost}>
+              {isLiked ? (
+                <AiFillHeart
+                  size={25}
+                  className="text-red-500 cursor-pointer hover:text-red-500/50"
+                />
+              ) : (
+                <AiOutlineHeart
+                  size={25}
+                  className="text-black cursor-pointer hover:text-black/50"
+                />
+              )}
             </div>
-          ))}
-        </div>
-      </div>
-      <div className="px-2">3 hours ago</div>
-      <div className="flex items-center px-2 py-4 mt-1 space-x-3 border-t border-gray-200">
-        <div>
-          <BsEmojiSmile className="text-xl" />
-        </div>
-        <form onSubmit={handlePostComment} className="flex w-full px-2">
-          <div className="w-full">
-            <input
-              type="text"
-              name={`comment ${id}`}
-              id={`comment ${id}`}
-              className="w-full bg-white outline-none"
-              placeholder="Add a comment..."
-              ref={comment}
-            />
+            <div>
+              <FaRegComment
+                size={22}
+                className="text-black cursor-pointer hover:text-black/50"
+              />
+            </div>
+            <div>
+              <IoShareOutline
+                size={22}
+                className="text-black cursor-pointer hover:text-black/50"
+              />
+            </div>
           </div>
           <div>
-            <button className="text-sm font-semibold text-blue-600">
-              Post
-            </button>
+            <BsBookmark
+              size={20}
+              className="text-black cursor-pointer hover:text-black/50"
+            />
           </div>
-        </form>
+        </div>
+        <div className="px-2">
+          {likesCount ? `${likesCount} likes` : "Be the first to like"}
+        </div>
+        <div className="px-2">{caption}</div>
+        <div className="p-2">
+          <div className="flex flex-col space-y-1">
+            {comments.map((commentData) => (
+              <div key={commentData.id} className="flex space-x-2">
+                <div className="font-medium">{commentData.username}</div>
+                <div>{commentData.comment}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="px-2">3 hours ago</div>
+        <div className="flex items-center px-2 py-4 mt-1 space-x-3 border-t border-gray-200">
+          <div>
+            <BsEmojiSmile className="text-xl" />
+          </div>
+          <form onSubmit={handlePostComment} className="flex w-full px-2">
+            <div className="w-full">
+              <input
+                type="text"
+                name={`comment ${id}`}
+                id={`comment ${id}`}
+                className="w-full bg-white outline-none"
+                placeholder="Add a comment..."
+                ref={comment}
+              />
+            </div>
+            <div>
+              <button className="text-sm font-semibold text-blue-600">
+                Post
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+
+      <ModalArticle
+        visible={visible}
+        onCloseModal={onCloseModal}
+        onOpenModal={onOpenModal}
+      >
+        <p>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old. Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure
+          Latin words, consectetur, from a Lorem Ipsum passage, and going
+          through the cites of the word in classical literature, discovered the
+          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+          1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the
+          theory of ethics, very popular during the Renaissance. The first line
+          of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in
+          section 1.10.32.
+        </p>
+        <p>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old. Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure
+          Latin words, consectetur, from a Lorem Ipsum passage, and going
+          through the cites of the word in classical literature, discovered the
+          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+          1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the
+          theory of ethics, very popular during the Renaissance. The first line
+          of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in
+          section 1.10.32.
+        </p>
+      </ModalArticle>
+    </>
   );
 };
 
